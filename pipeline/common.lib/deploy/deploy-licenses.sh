8bdd064dbccd615e3d2b09c7ed976aa3b227fdf8
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-11-07 04:34:26 +0530

    updates

--- pipeline/common.lib/deploy/deploy-licenses.sh
+++ pipeline/common.lib/deploy/deploy-licenses.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Load the utility functions
-. pipeline/common.lib/utils/get-version.sh
-kubectl config set-context --current --namespace=$2
-
 echo "Configurations will be applied in the below namespace-----"
 kubectl config view --minify | grep namespace:
 
@@ -10,8 +6,6 @@ kubectl config view --minify | grep namespace:
 cd licenses
 
 kubectl apply -f .
-
-#kubectl apply -f ../demo-multi-versions/
 
 echo "Describing the configurations"
 kubectl describe cm webmethodslicensekeys
