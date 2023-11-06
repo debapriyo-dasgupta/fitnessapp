b2e30f976a580a4be366a783136f5f78ec663b38
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-11-07 05:05:28 +0530

    updates

---
+++ pipeline/common.lib/deploy/deploy-fitness-memberships.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Load the utility functions
+. pipeline/common.lib/utils/get-version.sh
+
+echo "Configurations will be applied in the below namespace-----"
+kubectl config view --minify | grep namespace:
+
+# Deploy the environment specific configurations
+cd applications/memberships/app-manifests/
+
+read VERSION IMAGE_NAME < <(get-version "../version.txt")
+echo "IMAGE DETAILS - $IMAGE_NAME:$VERSION"
+
+# Deploy the target manifests to create the runtimes/pods
+sed -i "s/<TAG>/${VERSION}/g" memberships-deployment.yaml
+sed -i "s|<IMAGE_NAME>|${IMAGE_NAME}|g" memberships-deployment.yaml
+
+kubectl apply -f ../datastore
+kubectl apply -f .
+
+echo "Describing the configurations"
+kubectl describe deploy memberships
